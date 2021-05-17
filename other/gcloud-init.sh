e6e2f30ee4b048a41392b5f2b05ff0da520daa80
slefforge <57022538+slefforge@users.noreply.github.com>
2021-05-17 16:03:53 -0500

    removed unwanted local files

--- other/gcloud-init.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-source /Users/samlefforge/Documents/google-cloud-sdk/path.bash.inc
-source /Users/samlefforge/Documents/google-cloud-sdk/completion.bash.inc
-gcloud app deploy
-echo 'gcloud initialized'
