b6c09f542cb3200f42ee9ed23af7edfe80ff8463
slefforge <57022538+slefforge@users.noreply.github.com>
2021-05-26 13:19:06 -0500

    added a more efficient fill tool

--- static/drawing.js
+++ static/drawing.js
@@ -42,6 +42,7 @@ function draw(e) {
     ctx.lineCap = 'round'; // rounded end cap
 
     if (tool === 'pencil') {
+      console.log(color);
       ctx.strokeStyle = color; // hex color of line
     }
 
@@ -63,6 +64,8 @@ function colorsEqual(data1, data2) {
 function floodFill(x, y, originalColor) {
   const imageData = ctx.createImageData(1, 1);
   const data = imageData.data;
+  // this is why this breaks now, I'm no longer using the #ff0000 format, instead I would just call that 'red'
+  // not important because I'm redoing all of this
   data[0] = parseInt(color.substring(1, 3), 16);
   data[1] = parseInt(color.substring(3, 5), 16);
   data[2] = parseInt(color.substring(5, 7), 16);
@@ -95,8 +98,82 @@ function floodFill(x, y, originalColor) {
   }
 }
 
-function quickFill() {
+function colorPixel(pixelPos, imgData) {
+  imgData.data[pixelPos] = parseInt(color.substring(1, 3), 16);
+  imgData.data[pixelPos + 1] = parseInt(color.substring(3, 5), 16);
+  imgData.data[pixelPos + 2] = parseInt(color.substring(5, 7), 16);
+  imgData.data[pixelPos + 3] = 255;
+}
 
+function matchStartColor(imgData, pixelPos, startR, startG, startB) { // doesn't work yet, read the explanation on the guide
+  const r = imgData.data[pixelPos];
+  const g = imgData.data[pixelPos + 1];
+  const b = imgData.data[pixelPos + 2];
+  return (r === startR && g === startG && b === startB);
+}
+
+function quickFill(x, y) { // uses this algorithm: http://www.williammalone.com/articles/html5-canvas-javascript-paint-bucket-tool/
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  imgData.data[0] = 0;
+  ctx.putImageData(imgData, 0, 0);
+
+  x = Math.trunc(x); // You might not need
+  y = Math.trunc(y); // to do this
+
+  const pixelStack = [[x, y]];
+  let newPos;
+  let pixelPos = (y * canvas.width + x) * 4;
+  const startR = imgData.data[pixelPos];
+  const startG = imgData.data[pixelPos + 1];
+  const startB = imgData.data[pixelPos + 2];
+  let reachLeft;
+  let reachRight;
+
+  while (pixelStack.length) {
+    newPos = pixelStack.pop();
+    x = newPos[0];
+    y = newPos[1];
+
+    pixelPos = (y * canvas.width + x) * 4; // gives position if you numbered each square one dimensionally (kind of like a k-map)
+    while (y >= 0 && matchStartColor(imgData, pixelPos, startR, startG, startB)) { // going up
+      y -= 1;
+      pixelPos -= canvas.width * 4;
+    }
+
+    pixelPos += canvas.width * 4;
+    y += 1;
+    reachLeft = false;
+    reachRight = false;
+
+    while (y < canvas.height - 1 && matchStartColor(imgData, pixelPos, startR, startG, startB)) { // going down
+      y += 1;
+      colorPixel(pixelPos, imgData);
+      if (x > 0) { // left seeding
+        if (matchStartColor(imgData, pixelPos - 4, startR, startG, startB)) {
+          if (!reachLeft) {
+            pixelStack.push([x - 1, y]);
+            reachLeft = true;
+          }
+        } else if (reachLeft) {
+          reachLeft = false;
+        }
+      }
+
+      if (x < canvas.width - 1) { // right seeding
+        if (matchStartColor(imgData, pixelPos + 4, startR, startG, startB)) {
+          if (!reachRight) {
+            pixelStack.push([x + 1, y]);
+            reachRight = true;
+          }
+        } else if (reachRight) {
+          reachRight = false;
+        }
+      }
+      pixelPos += canvas.width * 4;
+    }
+  }
+  ctx.putImageData(imgData, 0, 0);
 }
 
 function click(e) {
@@ -104,8 +181,8 @@ function click(e) {
   if (tool === 'fill') {
     floodFill(pos.x, pos.y, ctx.getImageData(pos.x, pos.y, 1, 1).data);
   }
-  if (tool === 'fill2') {
-    quickFill();
+  if (tool === 'quickFill') {
+    quickFill(pos.x, pos.y);
   }
 }
 
